8198637b9392fdf724e7a651426377476bdd87d3
kutta97 <yhjin04170@gmail.com>
2025-04-01 01:08:44 +0900

    feat: renderElement 함수 추가

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,8 +3,22 @@ import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
+const oldNodeMap = new WeakMap();
+
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
+  const newNode = normalizeVNode(vNode);
+  const oldNode = oldNodeMap.get(container);
+
+  if (!oldNode) {
+    // 최초 렌더링시에는 createElement로 DOM을 생성하고
+    container.appendChild(createElement(newNode));
+  } else {
+    // 이후에는 updateElement로 기존 DOM을 업데이트한다.
+    updateElement(container, newNode, oldNode);
+  }
+
+  oldNodeMap.set(container, newNode);
+
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  setupEventListeners(container);
 }
