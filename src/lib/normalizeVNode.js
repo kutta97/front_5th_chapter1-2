e9504ee0b3677f159514ac9edc988572c3112783
kutta97 <yhjin04170@gmail.com>
2025-04-01 01:08:14 +0900

    feat: normalizeVNode 함수 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,22 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
+    return "";
+
+  if (typeof vNode === "string" || typeof vNode === "number")
+    return String(vNode);
+
+  if (vNode.children) {
+    vNode.children = vNode.children.map(normalizeVNode).filter(Boolean);
+  }
+
+  if (typeof vNode.type === "function") {
+    const normalized = normalizeVNode(vNode.type(vNode.props ?? {}));
+
+    return {
+      ...normalized,
+      children: [...normalized.children, ...vNode.children],
+    };
+  }
+
   return vNode;
 }
