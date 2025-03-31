db04501a9f86c4293f1ec597c3bb51d1b348db74
kutta97 <yhjin04170@gmail.com>
2025-04-01 01:07:57 +0900

    feat: createVNode 함수 추가

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,13 @@
+function isValidChild(child) {
+  if (typeof child === "boolean" && child === false) return false;
+  if (child === null || child === undefined) return false;
+  return true;
+}
+
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props: props,
+    children: children.flat(Infinity).filter(isValidChild),
+  };
 }
