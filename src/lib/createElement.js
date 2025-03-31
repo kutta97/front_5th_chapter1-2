8a98eeea4417b26270459978076b51e9509ff459
kutta97 <yhjin04170@gmail.com>
2025-04-01 01:08:24 +0900

    feat: createElement 함수 추가

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,51 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
+    return document.createTextNode("");
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+
+    const elements = vNode.map(createElement);
+    elements.forEach((element) => fragment.appendChild(element));
+
+    return fragment;
+  }
+
+  const element = document.createElement(vNode.type);
+
+  if (vNode.props) {
+    updateAttributes(element, vNode.props);
+  }
+
+  if (vNode.children && Array.isArray(vNode.children)) {
+    const children = vNode.children.map(createElement);
+
+    children.forEach((child) => element.appendChild(child));
+  }
+
+  return element;
+}
+
+function updateAttributes($el, props) {
+  Object.keys(props).forEach((key) => {
+    if (typeof props[key] === "function") {
+      const eventType = key.replace(/^on/i, "").toLowerCase();
+
+      addEvent($el, eventType, props[key]);
+      return;
+    }
+
+    if (key === "className") {
+      $el.setAttribute("class", props[key]);
+      return;
+    }
+
+    $el.setAttribute(key, props[key]);
+  });
+}
