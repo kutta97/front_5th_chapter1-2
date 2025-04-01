a5b73001d950f2bb31bb4cbbcef661418a7d7c5a
kutta97 <yhjin04170@gmail.com>
2025-04-01 23:38:09 +0900

    chore: baseUrl 빌드 환경 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,29 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default mergeConfig(
-  defineConfig({
-    esbuild: {
-      jsxFactory: "createVNode",
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        jsx: "transform",
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return mergeConfig(
+    defineConfig({
+      esbuild: {
         jsxFactory: "createVNode",
       },
-    },
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.js",
-      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-    },
-  }),
-);
+      optimizeDeps: {
+        esbuildOptions: {
+          jsx: "transform",
+          jsxFactory: "createVNode",
+        },
+      },
+      base: env.VITE_BASE_URL,
+    }),
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.js",
+        exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+      },
+    }),
+  );
+};
